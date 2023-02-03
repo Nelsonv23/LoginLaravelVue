9710ccee43facce237929c914ebdf9a7b0c60b0a
Nelson <nelsonv23@gmail.com>
2023-02-02 22:48:24 -0300

    Se agrega menú en archivo AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -34,6 +34,12 @@ const showingNavigationDropdown = ref(false);
                                 </NavLink>
                             </div>
                         </div>
+                          <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <NavLink :href="route('song.index')" :active="route().current('song.index')">
+                                    Dashboard
+                                </NavLink>
+                            </div>
+                        </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <!-- Settings Dropdown -->
